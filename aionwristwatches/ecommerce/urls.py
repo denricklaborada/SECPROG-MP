c627b4603a78f36a83cee5c98eca7c6bebae8443
Denrick Laborada <denricklaborada@users.noreply.github.com>
2018-02-17 22:47:44 +0800

    navbar

--- aionwristwatches/ecommerce/urls.py
+++ aionwristwatches/ecommerce/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
+from django.contrib.auth.views import logout
+from django.conf import settings
 from . import views
 
 app_name = 'ecommerce'
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^logout/$', logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
 ]
--- aionwristwatches/ecommerce/views.py
+++ aionwristwatches/ecommerce/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.views import login
 
 def index(request):
+	if request.method == 'POST':
+		return login(request, template_name="ecommerce/index.html")
 	return render(request, 'ecommerce/index.html')
