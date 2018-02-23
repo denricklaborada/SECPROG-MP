15a86fdb78cd8be2275002331b93101f36c554b7
kimmieco <kimberly_jane_co@dlsu.edu.ph>
2018-02-24 01:41:21 +0800

    changes

--- aionwristwatches/ecommerce/models.py
+++ aionwristwatches/ecommerce/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     description = models.CharField(max_length=100)
     price = models.DecimalField(default=0.00, max_digits=20, decimal_places=2,  validators=[MinValueValidator(0)])
     quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='ecommerce/static/product_images/', blank=True)
 
     def __str__(self):
         return self.prodName
