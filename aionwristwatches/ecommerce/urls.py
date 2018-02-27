fa9d8093cb4028e5b65d35fcdb1b3c0b3d2b26e5
kimmieco <kimberly_jane_co@dlsu.edu.ph>
2018-02-28 00:07:27 +0800

    new

--- aionwristwatches/ecommerce/urls.py
+++ aionwristwatches/ecommerce/urls.py
@@ -7,6 +7,8 @@ app_name = 'ecommerce'
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^(?P<product_id>[0-9]+)/cart/$', views.add_to_cart, name='addcart'),
     url(r'^checkout/$', views.checkout, name='checkout'),
+    url(r'^uacct/$', views.uacct, name='uacct'),
 	url(r'^logout/$', logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
 ]
--- aionwristwatches/ecommerce/views.py
+++ aionwristwatches/ecommerce/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,reverse
 from django.contrib.auth.views import login
+from django.http import HttpResponseRedirect
 from .forms import RegistrationForm
+from .models import Product
 
 def index(request):
 	if request.method == 'POST':
 		regform = RegistrationForm(request.POST)
+		print("REQUEST POST")
 		if regform.is_valid():
+			print("FORM VALID")
 			regform.save()
 			return redirect('/')
 
@@ -20,6 +24,18 @@ def index(request):
 	}
 	return render(request, 'ecommerce/index.html', context)
 
-def checkout(request):
+def add_to_cart(request, product_id):
+#     cart = request.session.get('cart',{})
+#     product = Product.objects.get(id=product_id)
+#     cart[product_id] = product
+#     request.session['cart'] = cart
+	return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+#Cart View
+def checkout(request):
+	##cart = request.session.get('cart', {})
 	return render(request, 'ecommerce/checkout.html')
+
+def uacct(request):
+
+	return render(request, 'ecommerce/uacct.html')
