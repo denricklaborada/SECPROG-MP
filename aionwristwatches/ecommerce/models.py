9f782739122e18f2752ecd34a91eb0ee35cd0dce
Denrick Laborada <denricklaborada@users.noreply.github.com>
2018-03-08 21:40:05 +0800

    added items left in index

--- aionwristwatches/ecommerce/models.py
+++ aionwristwatches/ecommerce/models.py
@@ -81,7 +81,7 @@ class Review(models.Model):
 
 class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    prodlist = models.ManyToManyField(Product, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     pubdate = models.DateTimeField(auto_now_add=True)
     subtotal =  models.DecimalField(default=0.00, max_digits=20, decimal_places=2,  validators=[MinValueValidator(0)])
 
